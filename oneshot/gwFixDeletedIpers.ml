4bde3a539af0394f033a91555cc2c0c9fd7d1835
Julien Sagot <julien.sagot@geneanet.org>
2020-05-27 09:03:09 +0200

    oneshot/gwFixDeletedIpers.ml: added printing

--- oneshot/gwFixDeletedIpers.ml
+++ oneshot/gwFixDeletedIpers.ml
@@ -7,13 +7,17 @@ let () =
   fun () ->
   let open Dbdisk in
   let base = Gwdb_driver_legacy.open_base bname in
-  let changes = ref false in
+  let changes = ref 0 in
   for i = 0 to base.data.persons.len - 1 do
     let p : (int, int, int) gen_person = base.data.persons.get i in
     if p.key_index <> i
     then begin
-      changes := true ;
+      incr changes ;
       Gwdb_driver_legacy.patch_person base i { p with key_index = i } ;
     end ;
   done ;
-  if !changes then Gwdb_driver_legacy.commit_patches base ;
+  if !changes <> 0 then begin
+    Gwdb_driver_legacy.commit_patches base ;
+    print_endline @@ "Commited " ^ string_of_int !changes ^ " changes."
+  end else
+    print_endline "No changes commited."
