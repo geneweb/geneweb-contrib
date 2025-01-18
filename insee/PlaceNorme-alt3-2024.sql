56c530c084c884e2e9e05f271c751937d72054cc
A2 <a2line@gmail.com>
2025-01-18 19:01:15 +0100

    manage mouvements of commune since 1943

--- insee/PlaceNorme-alt3-2024.sql
+++ insee/PlaceNorme-alt3-2024.sql
@@ -1,64 +1,68 @@
--- Version : "Commune (D�partement)" si homonyme, "Commune" sinon
+-- Version : "Commune (D?partement)" si homonyme, "Commune" sinon
+-- avec prise en compte des mouvements des communes depuis 1943
 
-DROP TEMPORARY TABLE IF EXISTS commune_homonymes;
-
+-- On crée une table normale au lieu d'une table temporaire pour les homonymes
+DROP TABLE IF EXISTS commune_homonymes;
 DROP TABLE IF EXISTS PlaceNorme;
 
+-- Table des homonymes (non temporaire)
+CREATE TABLE commune_homonymes (
+    Libelle VARCHAR(500) PRIMARY KEY
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Remplissage de la table des homonymes
+INSERT INTO commune_homonymes
+SELECT Libelle 
+FROM cog_mvt1943 
+GROUP BY Libelle 
+HAVING COUNT(DISTINCT Code) > 1;
+
+-- Affichage des statistiques sur les homonymes
+SELECT 'Nombre de libellés homonymes' as Description, COUNT(*) as Nombre 
+FROM commune_homonymes
+UNION ALL
+SELECT 'Nombre d''entrées concernées par les homonymes',
+    COUNT(*) 
+FROM cog_mvt1943 m
+INNER JOIN commune_homonymes h ON m.Libelle = h.Libelle;
+
+-- Création de la table finale
 CREATE TABLE PlaceNorme (
     Id INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     Code CHAR(5) NOT NULL,
-    DateFin CHAR(10) NOT NULL,
+    DateDebut DATE NOT NULL,
+    DateFin DATE DEFAULT NULL,
     Libelle VARCHAR(500),
-    INDEX I_Place_Code (Code)
-);
+    INDEX I_Place_Code (Code),
+    INDEX I_Place_Dates (DateDebut, DateFin)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Cr�ation table temporaire ici, juste avant son utilisation
-CREATE TEMPORARY TABLE commune_homonymes AS
-SELECT Libelle 
-FROM COG_commune 
-WHERE TypeCom = 'COM'
-GROUP BY Libelle 
-HAVING COUNT(*) > 1;
+-- Insertion avec gestion des homonymes
+INSERT INTO PlaceNorme (Code, DateDebut, DateFin, Libelle)
+SELECT 
+    m.Code,
+    m.DateDebut,
+    m.DateFin,
+    CASE 
+        WHEN h.Libelle IS NOT NULL AND c.Departement IS NOT NULL THEN
+            CONCAT(m.Libelle, ' (', d.Libelle, ')')
+        ELSE 
+            m.Libelle
+    END as Libelle
+FROM cog_mvt1943 m
+LEFT JOIN commune_homonymes h ON m.Libelle = h.Libelle
+LEFT JOIN COG_commune c ON m.Code = c.Code
+LEFT JOIN COG_departement d ON c.Departement = d.Code;
 
-CREATE INDEX idx_libelle ON commune_homonymes(Libelle);
+-- Statistiques finales
+SELECT 'Nombre total de lignes après jointures' as Description, COUNT(*) as Nombre 
+FROM PlaceNorme
+UNION ALL
+SELECT 'Nombre de codes uniques après jointures', COUNT(DISTINCT Code) 
+FROM PlaceNorme
+UNION ALL
+SELECT 'Nombre de libellés uniques après jointures', COUNT(DISTINCT Libelle) 
+FROM PlaceNorme;
 
-insert into PlaceNorme
-select null, c.Code, '2050-01-01', 
-       case 
-         when h.Libelle is not null 
-         then concat(c.Libelle, ' (', d.Libelle, ')')
-         else c.Libelle
-       end
-from COG_commune c
-inner join COG_departement d on c.Departement = d.Code
-left join commune_homonymes h on c.Libelle = h.Libelle
-where c.TypeCom = "COM";
-
-insert into PlaceNorme
-select null, c.Code, '2050-01-01', concat_ws(', ', c.Libelle, n.Libelle)
-from COG_commune c
-inner join PlaceNorme n on c.Parent = n.Code
-where c.TypeCom in ("COMA","COMD");
-
-insert into PlaceNorme
-select null, c.Code, '2050-01-01',
- concat(
- substr(c.Libelle, 1, locate(' ', c.Libelle)),
- lpad(
-  substr(c.Libelle, locate(' ', c.Libelle)+1, locate('e', substr(c.Libelle, locate(' ', c.Libelle)+1))-1),
-  2, '0'), ', ', n.Libelle )
-from COG_commune c
-inner join PlaceNorme n on c.Parent = n.Code
-where TypeCom = "ARM";
-
-insert into PlaceNorme
-select null, m.AvCode, m.DateMvt, n.Libelle
-from COG_mvt m
-inner join PlaceNorme n on m.ApCode = n.Code
-where TypeMvt = "41";
-
-insert into PlaceNorme
-select null, m.AvCode, m.DateMvt, concat(m.AvLibelle, ', ', n.Libelle)
-from COG_mvt m
-inner join PlaceNorme n on m.APCode = n.Code
-where TypeMvt in ("31", "32", "33") and AvLibelle <> ApLibelle;
+-- Nettoyage
+DROP TABLE commune_homonymes;
